8ca4ec80aa03a2b54a8f7c85a61c1b39d8d7fbae
Yash Upadhyay <quic_yupadhya@quicinc.com>
2024-01-05 13:40:58 +0530

    msm: camera: cdm: check irq status on hang detection
    Problem:
    Check IRQ status on hang detection if the inline IRQ
    is set then the cdm has triggered IRQ but there is a
    workqueue scheduling delay which is causing the cdm's
    config timeout.
    
    Solution:
    To prevent the timeout due to
    scheduling delay check the work record and irq status
    and return true if its delay.
    
    CRs-Fixed: 3433175
    Change-Id: Iaa34f8ff9b57e7da9f80677a7da9b4f9a53dad14
    Signed-off-by: Yash Upadhyay <quic_yupadhya@quicinc.com>

--- drivers/cam_isp/isp_hw_mgr/cam_tfe_hw_mgr.c
+++ drivers/cam_isp/isp_hw_mgr/cam_tfe_hw_mgr.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/slab.h>
@@ -2974,6 +2974,7 @@ static int cam_tfe_mgr_config_hw(void *hw_mgr_priv,
 	struct cam_tfe_hw_mgr_ctx *ctx;
 	struct cam_isp_prepare_hw_update_data *hw_update_data;
 	bool is_cdm_hung = false;
+	unsigned long rem_jiffies = 0;
 
 	if (!hw_mgr_priv || !config_hw_args) {
 		CAM_ERR(CAM_ISP, "Invalid arguments");
@@ -3154,12 +3155,13 @@ static int cam_tfe_mgr_config_hw(void *hw_mgr_priv,
 		goto end;
 
 	for (i = 0; i < CAM_TFE_HW_CONFIG_WAIT_MAX_TRY; i++) {
-		rc = cam_common_wait_for_completion_timeout(
+		rem_jiffies = cam_common_wait_for_completion_timeout(
 			&ctx->config_done_complete,
 			msecs_to_jiffies(
 			CAM_TFE_HW_CONFIG_TIMEOUT));
-		if (rc <= 0) {
-			if (!cam_cdm_detect_hang_error(ctx->cdm_handle)) {
+		if (rem_jiffies <= 0) {
+			rc = cam_cdm_detect_hang_error(ctx->cdm_handle);
+			if (rc == 0) {
 				CAM_ERR(CAM_ISP,
 					"CDM workqueue delay detected, wait for some more time req_id=%llu rc=%d ctx_index %d",
 					cfg->request_id, rc,
@@ -3171,24 +3173,21 @@ static int cam_tfe_mgr_config_hw(void *hw_mgr_priv,
 					CAM_DEFAULT_VALUE,
 					CAM_DEFAULT_VALUE, rc);
 				continue;
-			}
+			} else {
+				CAM_ERR(CAM_ISP,
+					"cfg_done completn timeout cdm_hang=%d req=%llu ctx_idx=%d",
+					true, cfg->request_id,
+					ctx->ctx_index);
+				cam_req_mgr_debug_delay_detect();
+				trace_cam_delay_detect("ISP",
+					"config done completion timeout",
+					cfg->request_id, ctx->ctx_index,
+					CAM_DEFAULT_VALUE, CAM_DEFAULT_VALUE,
+					rc);
 
-			CAM_ERR(CAM_ISP,
-				"config done completion timeout for req_id=%llu rc=%d ctx_index %d",
-				cfg->request_id, rc,
-				ctx->ctx_index);
-
-			cam_req_mgr_debug_delay_detect();
-			trace_cam_delay_detect("ISP",
-				"config done completion timeout",
-				cfg->request_id, ctx->ctx_index,
-				CAM_DEFAULT_VALUE, CAM_DEFAULT_VALUE,
-				rc);
-
-			if (rc == 0)
 				rc = -ETIMEDOUT;
-
-			goto end;
+				break;
+			}
 		} else {
 			rc = 0;
 			if (hw_update_data->mup_en)
@@ -3201,7 +3200,7 @@ static int cam_tfe_mgr_config_hw(void *hw_mgr_priv,
 	}
 
 	if ((i == CAM_TFE_HW_CONFIG_WAIT_MAX_TRY) && (rc == 0))
-		rc = -ETIMEDOUT;
+		CAM_DBG(CAM_ISP, "Wq delayed but IRQ CDM done");
 
 end:
 	CAM_DBG(CAM_ISP, "Exit: Config Done: %llu",  cfg->request_id);
