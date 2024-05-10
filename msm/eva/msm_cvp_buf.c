42aa7a59f6a0fc2ab22b35c9c037450c9426c1b3
Gopireddy Arunteja Reddy <quic_garuntej@quicinc.com>
2024-05-10 14:41:32 +0530

    msm: eva: UAF issue in mapping wncc buffers
    Adding wncc buf only if the wncc buf count doesn't exceed
    the 'EVA_KMD_WNCC_MAX_SRC_BUFS'.
    
    Change-Id: Icfe814ad19b776f8f1f83ee6a1839984d1b227ac
    Signed-off-by: Gopireddy Arunteja Reddy <quic_garuntej@quicinc.com>

--- msm/eva/msm_cvp_buf.c
+++ msm/eva/msm_cvp_buf.c
@@ -508,11 +508,11 @@ int msm_cvp_map_buf_wncc(struct msm_cvp_inst *inst,
 		}
 	}
 
-	list_add_tail(&cbuf->list, &inst->cvpwnccbufs.list);
 	for (i = 0; i < EVA_KMD_WNCC_MAX_SRC_BUFS; i++)
 	{
 		if (inst->cvpwnccbufs_table[i].iova == 0)
 		{
+			list_add_tail(&cbuf->list, &inst->cvpwnccbufs.list);
 			inst->cvpwnccbufs_num++;
 			inst->cvpwnccbufs_table[i].fd = buf->fd;
 			inst->cvpwnccbufs_table[i].iova = smem->device_addr;
