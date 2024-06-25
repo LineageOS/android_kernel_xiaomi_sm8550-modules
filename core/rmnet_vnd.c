d8797702f84aedf92b2b74250b6628ea360c3dfe
Sharath Chandra Vurukala <quic_sharathv@quicinc.com>
2024-06-25 15:44:24 +0530

    datarmnet: Update the license markings correctly.
    Gerrit UI propagated change got merged, though there
    were conflicts in the markings. Correcting it.
    
    Change-Id: I3db8b4a0461aafe59692024755abf0eaa4186bdc

--- core/rmnet_vnd.c
+++ core/rmnet_vnd.c
@@ -1,9 +1,6 @@
 /* Copyright (c) 2013-2021, The Linux Foundation. All rights reserved.
-<<<<<<< HEAD   (e467bf datarmnet: Enabled datarmnet for trinket IOT)
- * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
-=======
- * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
->>>>>>> CHANGE (f58365 core: Increase the gso max size for rmnet)
+ * Copyright (c) 2021-2022, 2024 Qualcomm Innovation Center, Inc.
+ * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
