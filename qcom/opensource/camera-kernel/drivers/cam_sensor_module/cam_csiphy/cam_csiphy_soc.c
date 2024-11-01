2fe3daed884388cc27327a56c5855df4fd07505f
danielml <daniel@danielml.dev>
2024-11-01 16:06:08 +0100

    camera-kernel: Add M11 csiphy support
    Change-Id: Ib718e2082629115f033d3f5ce2f33ceae517ecf5
    Signed-off-by: danielml <daniel@danielml.dev>

--- qcom/opensource/camera-kernel/drivers/cam_sensor_module/cam_csiphy/cam_csiphy_soc.c
+++ qcom/opensource/camera-kernel/drivers/cam_sensor_module/cam_csiphy/cam_csiphy_soc.c
@@ -324,6 +324,11 @@ int32_t cam_csiphy_parse_dt_info(struct platform_device *pdev,
 		csiphy_dev->hw_version = CSIPHY_VERSION_V220;
 		csiphy_dev->is_divisor_32_comp = true;
 		csiphy_dev->clk_lane = 0;
+	} else if (of_device_is_compatible(soc_info->dev->of_node, "qcom,csiphy-v2.1.2-m11")) {
+		csiphy_dev->ctrl_reg = &ctrl_reg_2_1_2_m11;
+		csiphy_dev->hw_version = CSIPHY_VERSION_V212;
+		csiphy_dev->is_divisor_32_comp = true;
+		csiphy_dev->clk_lane = 0;
 	} else {
 		CAM_ERR(CAM_CSIPHY, "invalid hw version : 0x%x",
 			csiphy_dev->hw_version);
