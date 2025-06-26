73432b6bfc642e32ab77212b2a9cbc8b20fd0582
Lunark :3 <jvillaltamatos3007@gmail.com>
2025-06-26 12:26:22 -0500

    camera-kernel: Add N11 csiphy support
    Change-Id: Ied8c0c4aa5df866f6aacd5fe2950d105ae130c4b
    Signed-off-by: Lunark :3 <jvillaltamatos3007@gmail.com>

--- qcom/opensource/camera-kernel/drivers/cam_sensor_module/cam_csiphy/cam_csiphy_soc.c
+++ qcom/opensource/camera-kernel/drivers/cam_sensor_module/cam_csiphy/cam_csiphy_soc.c
@@ -329,6 +329,11 @@ int32_t cam_csiphy_parse_dt_info(struct platform_device *pdev,
 		csiphy_dev->hw_version = CSIPHY_VERSION_V212;
 		csiphy_dev->is_divisor_32_comp = true;
 		csiphy_dev->clk_lane = 0;
+	} else if (of_device_is_compatible(soc_info->dev->of_node, "qcom,csiphy-v2.1.2-n11")) {
+		csiphy_dev->ctrl_reg = &ctrl_reg_2_1_2_n11;
+		csiphy_dev->hw_version = CSIPHY_VERSION_V212;
+		csiphy_dev->is_divisor_32_comp = true;
+		csiphy_dev->clk_lane = 0;
 	} else {
 		CAM_ERR(CAM_CSIPHY, "invalid hw version : 0x%x",
 			csiphy_dev->hw_version);
